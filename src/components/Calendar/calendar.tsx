e805a79fb3bda43d5318c1bdd79b7deaef3e9bd1
anabellinzona <143641800+anabellinzona@users.noreply.github.com>
2026-03-21 11:20:38 -0400

    create the help and create component and the style

--- src/components/Calendar/calendar.tsx
+++ src/components/Calendar/calendar.tsx
@@ -5,19 +5,50 @@ import WelcomeText from '../WelcomeText/welcomeText'
 import { useState } from 'react'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
+import CreateAppointment from './CreateAppointment/createAppointment'
+import Help from './Help/help'
 
 export default function CalendarioMedico() {
   const [mostrarMiniCalendario, setMostrarMiniCalendario] = useState(false);
   const [fechaSeleccionada, setFechaSeleccionada] = useState<Date | null>(new Date());
 
+  const [showForm, setShowForm] = useState(false);
+
+  const [mostrarInformacionDeAyuda, setMostrarInformacionDeAyuda] = useState(false);
+
   const turnos = [
     { title: 'Dro. Juan Pérez', start: '2026-03-20T09:00:00' },
     { title: 'Dro. Ana García', start: '2026-03-20T10:00:00' },
-     { title: 'Dro. Ana García', start: '2026-03-19T08:00:00' },
+    { title: 'Dro. Ana García', start: '2026-03-19T08:00:00' },
   ]
 
   return (
     <div className={styles.calendarProperties}>
+      {mostrarMiniCalendario && (
+          <div className={styles.miniCalendarProperties}>
+            <DatePicker
+              selected={fechaSeleccionada}
+              onChange={(fecha: Date | null) => {
+                if (fecha) {
+                  setFechaSeleccionada(fecha)
+                  setMostrarMiniCalendario(false)
+                }
+              }}
+              inline
+            />
+          </div>
+        )}
+      {mostrarInformacionDeAyuda && (
+        <div className={styles.miniCalendarProperties}>
+           <Help></Help>
+        </div>
+      )}
+      
+      {showForm && (
+        <div>
+          <CreateAppointment name='Ana' onClose={() => setShowForm(false)}></CreateAppointment>
+        </div>
+      )}
       <div className={styles.calendarContainerProperties}>
         <WelcomeText sectionText='Aca el calendario de la semana' className='darkStyle'></WelcomeText>
         <div className={styles.calendarAndButtonsContainerProperties}>
@@ -49,30 +80,16 @@ export default function CalendarioMedico() {
           </div>
           <div className={styles.buttonsContainerProperties}>
             <div className={styles.newAppointment}>
-              <button>+</button>
+              <button onClick={() => setShowForm(true)}>+</button>
             </div>
             <div className={styles.buttonsSectionProperties}>
               <button><img src='/icons/label.png'></img></button>
-              <button onClick={() => setMostrarMiniCalendario(true)}><img src='/icons/calendar.png'></img></button>
+              <button onClick={() => setMostrarMiniCalendario(!mostrarMiniCalendario)}><img src='/icons/calendar.png'></img></button>
               <button><img src='/icons/settings.png'></img></button>
-              <button><img src='/icons/info.png'></img></button>
+              <button onClick={() => setMostrarInformacionDeAyuda(!mostrarInformacionDeAyuda)}><img src='/icons/info.png'></img></button>
             </div>
           </div>
         </div>
-        {mostrarMiniCalendario && (
-          <div className={styles.miniCalendarProperties}>
-            <DatePicker
-              selected={fechaSeleccionada}
-              onChange={(fecha: Date | null) => {
-                if (fecha) {
-                  setFechaSeleccionada(fecha)
-                  setMostrarMiniCalendario(false)
-                }
-              }}
-              inline
-            />
-          </div>
-        )}
       </div>
     </div>
   )
