922183650b672733df6c15157b4b7b5b02f7d6ee
anabellinzona <143641800+anabellinzona@users.noreply.github.com>
2026-04-17 17:20:00 -0300

    finish the login section

--- src/components/Login/login.tsx
+++ src/components/Login/login.tsx
@@ -1,11 +1,15 @@
 import { authService } from "../../services/auth.service";
 import { useState } from "react";
-import { userService } from "../../services/user.service";
 import type { LoginRequestDTO } from "../../types/auth.types";
-import WelcomeText from "../WelcomeText/welcomeText";
 import styles from './login.module.css'
+import { useNavigate } from "react-router-dom";
 
-export default function Login() {
+type Prop = {
+    onLogin: () => void
+}
+
+export default function Login({onLogin} : Prop) {
+    const navigate = useNavigate()
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
@@ -22,6 +26,8 @@ export default function Login() {
 
             console.log("Voy a logearme la task");
             response = await authService.login(login);
+            onLogin()
+            navigate('/Panel')
             console.log("Logueado")
 
         } catch (error: any) {
@@ -29,30 +35,31 @@ export default function Login() {
         }
     }
     return (
-        <div className={styles.firstContainerProperties}>
-            <div className={styles.formContainerProperties}>
-                <form className={styles.formProperties} onSubmit={handleSumbit}>
-                    <div className={styles.labelAndInputProperties}>
-                        <div className={styles.labelContainerProperties}>
-                            <label>Usuario *</label>
+        <div className={styles.containerProperties}>
+            <div className={styles.firstContainerProperties}>
+                <div className={styles.formContainerProperties}>
+                    <form className={styles.formProperties} onSubmit={handleSumbit}>
+                        <div className={styles.labelAndInputProperties}>
+                            <div className={styles.labelContainerProperties}>
+                                <label>Usuario *</label>
+                            </div>
+                            <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Nombre de usuario" />
                         </div>
-                        <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Nombre de usuario" />
-                    </div>
-                    <div className={styles.labelAndInputProperties}>
-                        <div className={styles.labelContainerProperties}>
-                            <label>Contraseña *</label>
+                        <div className={styles.labelAndInputProperties}>
+                            <div className={styles.labelContainerProperties}>
+                                <label>Contraseña *</label>
+                            </div>
+                            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Contraseña" />
                         </div>
-                        <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Contraseña" />
-                    </div>
-                    <div className={styles.buttonProperties}>
-                        <button>Iniciar sesión</button>
-                    </div>
-                </form>
+                        <div className={styles.buttonProperties}>
+                            <button>Iniciar sesión</button>
+                        </div>
+                    </form>
+                </div>
+                <div className={styles.imageProperties}>
+                    <img src="/logo/whiteLogo.png" width={370} height={150}></img>
+                </div>
             </div>
-            <div className={styles.imageProperties}>
-                <img src="/logo/whiteLogo.png" width={370} height={150}></img>
-            </div>
-
         </div>
     )
 }
